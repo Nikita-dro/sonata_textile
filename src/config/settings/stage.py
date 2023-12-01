c71be526904205c8d9022a9217ede452bcdb75a6
mykyta <drobotda598@gmail.com>
2023-12-01 08:29:10 +0000

    Added dev, stage and prod

---
+++ src/config/settings/stage.py
@@ -0,0 +1,19 @@
+from config.settings.base_settings import BASE_DIR
+
+SECRET_KEY = "django-insecure-uxe#&&$9t7&x%xq*9ur)+b9a08t0pygm6*523v%+2j#ft2yq1m"
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
